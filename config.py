e69b8e1563c86638cde920a5092a6ef996c86d23
MIPOHBOPOHIH <98014624+MIPOHBOPOHIH@users.noreply.github.com>
2023-07-08 22:29:01 +0300

    чуть обновил конфиг

--- config.py
+++ config.py
@@ -2,6 +2,7 @@ BOT_TOKEN = ''  # Токен от @BotFather
 YANDEX_MUSIC_TOKEN = ''  # Токен от Yandex-music-api
 YOUR_CHANNEL = '@EXAMPLECHANNEL'  # Id канала/Username канала, куда надо отправлять текст. Пример: '@examplechannel'
 YOUR_URL = 'https://t.me/BOT_USERNAME?start'  # Вместо BOT_USERNAME вставьте username вашего бота. Пример: https://t.me/testbot?start
+# добавлять значения ниже необязательно. в случае, если вы оставите их пустыми, получение треков с last.fm не будет работать 
 LASTFM_API_KEY = ''  # Api-key от last.fm
 LASTFM_API_SECRET = ''  # Secret api-key от last.fm
 LASTFM_USERNAME = ''  # Ваш username на last.fm
